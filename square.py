2ebec9ecb97ccdfcb195dd2834ae2f759d1effc9
Artem <me.peremy@gmail.com>
2023-11-22 00:40:21 +0300

    test: add test for square.py

--- square.py
+++ square.py
@@ -1,3 +1,24 @@
+import unittest
+
+class SquareTestCase(unittest.TestCase):
+    def test_square_Area_Zero(self):
+        self.assertRaises(ValueError, area, 0)
+
+    def test_square_area(self):
+        self.assertEquals(area(4), 16)
+
+    def test_square_negative_area(self):
+        self.assertRaises(ValueError, area, -123)
+
+    def test_square_perimeter(self):
+        self.assertEquals(perimeter(23), 92)
+
+    def test_square_perimeter_zero(self):
+        self.assertRaises(ValueError, perimeter, 0)
+
+    def test_square_negative_perimeter(self):
+        self.assertRaises(ValueError, perimeter, -4)
+
 def area(a):
     '''Принимает число a, возвращает квадрат этого числа'''
     return a * a
